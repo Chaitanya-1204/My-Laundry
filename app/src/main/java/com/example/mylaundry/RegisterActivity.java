3f72de176c691d1a8fb4c27762eeb2207c2bd8e7
Chaitanya <chaitanyapatil1204@gmail.com>
2021-05-06 02:07:34 +0530

    added registration,java file

--- app/src/main/java/com/example/mylaundry/RegisterActivity.java
+++ app/src/main/java/com/example/mylaundry/RegisterActivity.java
@@ -4,18 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
-    private TextView Login;
+    private TextView Login ,banner , registerUser ;
+    private EditText FullName , Phone , Email , Password;
+
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
         Login = (TextView) findViewById(R.id.Login);
         Login.setOnClickListener(this);
+        
+        banner = (TextView) findViewById(R.id.banner);
+        banner.setOnClickListener(this);
+
+        registerUser = (Button) findViewById(R.id.registerUser);
+        registerUser.setOnClickListener(this);
+
+        FullName = (EditText) findViewById(R.id.fullName);
+        Phone = (EditText) findViewById(R.id.phoneNumber);
+        Email = (EditText) findViewById(R.id.email);
+        Password = (EditText) findViewById(R.id.password);
+
+
+
+        mAuth = FirebaseAuth.getInstance();
 
 
 
@@ -27,7 +50,61 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         switch (v.getId()){
             case R.id.Login:
                 startActivity(new Intent(this , LoginActivity.class));
+                break;
+            case R.id.banner:
+                startActivity(new Intent(this , LoginActivity.class));
+                break;
+            case R.id.registerUser:
+                registeredUser();
+                break;
         }
+
+    }
+
+    private void registeredUser() {
+        String email = Email.getText().toString().trim();
+        String fullName = FullName.getText().toString().trim();
+        String password = Password.getText().toString().trim();
+        String phone_number = Phone.getText().toString().trim();
+
+        if(fullName.isEmpty()){
+            FullName.setError("Name is Required");
+            FullName.requestFocus();
+            return;
+        }
+        if(email.isEmpty()){
+            Email.setError("Email is Required");
+            Email.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            Email.setError("Enter a valid Mail");
+            Email.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            Password.setError("Password is Required");
+            Password.requestFocus();
+            return;
+        }
+        if(phone_number.isEmpty()){
+            Phone.setError("Phone Number is required is Required");
+            Phone.requestFocus();
+            return;
+        }
+        if (password.length() < 6){
+            Password.setError("Password must be 6 or more Character Long");
+            Password.requestFocus();
+            return;
+
+        }
+        if(phone_number.length() != 10){
+            Phone.setError("Invalid Phone Number");
+            Phone.requestFocus();
+            return;
+        }
+
+
 
     }
 }
