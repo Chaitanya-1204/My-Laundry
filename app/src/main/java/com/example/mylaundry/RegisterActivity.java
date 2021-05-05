bc6fef8f9c6322c98056dd75ce45a5c6e8dee4e7
Chaitanya <chaitanyapatil1204@gmail.com>
2021-05-06 00:45:08 +0530

    Added registration UI and added the intent for going from login activity to register activity and vice versa

--- app/src/main/java/com/example/mylaundry/RegisterActivity.java
+++ app/src/main/java/com/example/mylaundry/RegisterActivity.java
@@ -7,21 +7,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        Button Register = (Button) findViewById(R.id.Register_Button);
-        Register.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_register);
+        Button Login = (Button) findViewById(R.id.Login_Button);
+        Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
+                Intent intent=new Intent(RegisterActivity.this,LoginActivity.class);
                 startActivity(intent);
 
             }
         });
 
+
+
     }
+
 }
