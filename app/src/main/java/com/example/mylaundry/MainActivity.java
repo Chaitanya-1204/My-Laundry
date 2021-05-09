ca1cd778dd4200e1f6a17fc755a159ba403b2134
Chaitanya <chaitanyapatil1204@gmail.com>
2021-05-10 00:04:33 +0530

    Added fragments in the front page

--- app/src/main/java/com/example/mylaundry/MainActivity.java
+++ app/src/main/java/com/example/mylaundry/MainActivity.java
@@ -1,18 +1,62 @@
 package com.example.mylaundry;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.view.MenuItem;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import com.example.mylaundry.menu.home.HomeFragment;
+import com.example.mylaundry.menu.order.OrderFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+
+
+
     }
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
+
+        Fragment selectedFragment = null;
+        switch (item.getItemId()) {
+            case R.id.nav_menu_home:
+                selectedFragment = new HomeFragment();
+                break;
+            case R.id.nav_menu_orders:
+                selectedFragment = new OrderFragment();
+                break;
+
+        }
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, selectedFragment)
+                .commit();
+        return true;
+    };
+
+
+
 
 
 }
