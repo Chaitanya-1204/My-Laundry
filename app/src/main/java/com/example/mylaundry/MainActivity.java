645e9f01576fb2665ad2886d96971f3a3f34cf97
Chaitanya <chaitanyapatil1204@gmail.com>
2021-05-10 01:37:51 +0530

    Added fragments and navigation bar

--- app/src/main/java/com/example/mylaundry/MainActivity.java
+++ app/src/main/java/com/example/mylaundry/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.mylaundry;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import com.example.mylaundry.menu.home.HomeFragment;
@@ -15,7 +18,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationMenu;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+
 
 
 
@@ -30,6 +34,21 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this , drawerLayout , toolbar,
+                R.string.navigation_drawer_open , R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.setDrawerIndicatorEnabled(false);
+        toggle.setToolbarNavigationClickListener(view -> drawerLayout.openDrawer(GravityCompat.START));
+        toggle.setHomeAsUpIndicator(R.drawable.ic_menu);
+        toggle.syncState();
+
 
 
 
@@ -55,8 +74,15 @@ public class MainActivity extends AppCompatActivity {
         return true;
     };
 
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
 
+        }
 
-
-
+    }
 }
